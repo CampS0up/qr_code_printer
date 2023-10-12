b5f00e58e12e445f2d78fbc0285d9e811197054e
CBrown <cbrown@neptunetg.com>
2023-10-12 10:09:46 -0500

    Create a script that will install all needed libraries to enable easy deployment

---
+++ base_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update package list
+echo "Updating package lists..."
+sudo apt-get update -y
+
+# Install Python 3 and pip
+echo "Installing Python 3 and pip..."
+sudo apt-get install -y python3 python3-pip
+
+# Install QR code library
+echo "Installing qrcode library..."
+pip3 install qrcode[pil]
+
+# Install XlsxWriter library
+echo "Installing XlsxWriter library..."
+pip3 install xlsxwriter
